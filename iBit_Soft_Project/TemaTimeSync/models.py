cc651ef15408d5928defa22e14e72da13d16bba6
wazaaah <mcnobert1amoah@gmail.com>
2024-08-27 08:45:48 +0000

    Second Upload

--- iBit_Soft_Project/TemaTimeSync/models.py
+++ iBit_Soft_Project/TemaTimeSync/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=125)
+    last_name = models.CharField(max_length=125)
+    username = models.CharField(max_length=120)
+    email = models.CharField(max_length=120)
